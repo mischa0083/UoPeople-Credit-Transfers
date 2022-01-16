c4d8d0fc8263fe5357070f81a9e92e8a1ec41d31
Mischa <mischa0083@github.com>
2022-01-15 17:52:00 -0800

    Push credit transfer website table v1

---
+++ src/Home.js
@@ -0,0 +1,61 @@
+import data from './data.json'
+
+const Home = () => {
+
+    return (
+        <div className="home">
+            <p>This site documents transferable options to taking courses at UoPeople.</p>
+            <table className="styled-table">
+                <thead>
+                    <tr>
+                        <th>Course Code</th>
+                        <th>Course Name</th>
+                        <th>Vendor</th>
+                        <th>Vendor Course</th>
+                        <th>Valid Dates</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.keys(data).map(function (key, index) {
+                        return (
+                            data[key].transferable_credits.map(function (key2) {
+                                return (
+                                    <tr key={index} className="active-row">
+                                        <td>
+                                            {key}
+                                        </td>
+                                        <td>
+                                            <a href={data[key].url}>
+                                                {data[key].name}
+                                            </a>
+                                        </td>
+                                        <td>
+                                            {key2.institution}
+                                        </td>
+                                        <td>
+                                            <a href={key2.url}>
+                                                {key2.name}
+                                            </a>
+                                        </td>
+                                        <td>
+                                            <a href={key2.ace_url}>
+                                                {key2.ace_valid_dates[0].start_date} - {key2.ace_valid_dates[0].end_date}
+                                            </a>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        
+                            )
+                    })
+                    }
+                </tbody>
+            </table>
+            <br/>
+            Have a course you want to add? <a href="https://github.com/mischa0083/UoPeople-Credit-Transfers">Open an Issue or a Pull Request</a>, <i>and please make sure to attach proof (i.e. transcripts, etc.)</i>.
+        </div>
+
+    );
+}
+
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>UoPeople Credit Transfers</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="https://github.com/mischa0083/UoPeople-Credit-Transfers/">GitHub Repository</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
